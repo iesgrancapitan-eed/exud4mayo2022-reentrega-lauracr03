f188bd97f4db876c7ffb760d8f53786070eb6fb2
Laura Caballero <a21carula@iesgrancapitan.org>
2022-06-08 17:05:24 +0200

    refactor: mensaje de la excepción

--- src/com/lcr/eed/examen/OptimizacionYDocumentacion/EdadErroneaException.java
+++ src/com/lcr/eed/examen/OptimizacionYDocumentacion/EdadErroneaException.java
@@ -2,7 +2,7 @@ package com.lcr.eed.examen.OptimizacionYDocumentacion;
 
 public class EdadErroneaException extends Exception {
 
-	public EdadErroneaException(String string) {
-		super(string);
+	public EdadErroneaException(String message) {
+		super(message);
 	}
 }
